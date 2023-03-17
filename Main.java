29dfa52688d954d83a130a65d601362a06655a8e
YULIAMAREVALO <123769066+YULIAMAREVALO@users.noreply.github.com>
2023-03-17 16:22:10 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+class Paciente {
+    private String nombre;
+    private int edad;
+    private String afiliacion;
+    private boolean condicionEspecial;
+
+    public Paciente(String nombre, int edad, String afiliacion, boolean condicionEspecial) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.afiliacion = afiliacion;
+        this.condicionEspecial = condicionEspecial;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public String getAfiliacion() {
+        return this.afiliacion;
+    }
+
+    public boolean getCondicionEspecial() {
+        return this.condicionEspecial;
+    }
+    restri
+}  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+
+
